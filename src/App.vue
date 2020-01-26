b50da04998389bd47f884fd07d35cc8bcce355e2
lauren <lauren.lum50@gmail.com>
2020-01-26 03:00:00 -0500

    yay

--- src/App.vue
+++ src/App.vue
@@ -19,21 +19,120 @@
     <audio id="test6"> 
       <source src="./Lhr .mp3">
     </audio>
+    <audio id="hoesmad"> 
+      <source src="https://drive.google.com/uc?export=download&id=1M4SmVtLIxvrF_Vq0XKj5ZHP7faulVIBd">
+    </audio>
 
-    <!-- jh audio -->
-    <audio id="jh1"> 
+    <!-- dumbass audio -->
+    <audio id="cj1"> 
       <source src="https://drive.google.com/uc?export=download&id=1kHbNp0Leq9pCzBMYL9c1kqIKzt9xR1Xz">
     </audio>
-    <audio id="jh2"> 
+    <audio id="cj2"> 
       <source src="https://drive.google.com/uc?export=download&id=1kGXC-grVmr9_EXpz4qM3y02nCUvTeeZB">
     </audio>
-    <audio id="jh3"> 
+    <audio id="cj3"> 
       <source src="https://drive.google.com/uc?export=download&id=1_UycGOB0JUkzBEqQHYIBZgIT8JXeRxpx">
     </audio>
-    <audio id="jh4"> 
+    <audio id="cj4"> 
       <source src="https://drive.google.com/uc?export=download&id=1523U6W7YdMERuYiL1CgC-n8Ak7Gdjhm3">
     </audio>
+    <audio id="cj5"> 
+      <source src="https://drive.google.com/uc?export=download&id=1ypRFdWQQMDkHTkQNZay89KmOPchEwEfu">
+    </audio>
+    <audio id="cj6"> 
+      <source src="https://drive.google.com/uc?export=download&id=1k77txrX0MKE85E5DtYm-mflJIR9_ZVLQ">
+    </audio>
+    <audio id="cj7"> 
+      <source src="https://drive.google.com/uc?export=download&id=1jtN8ghDGfFNilq3UBamVA06b-aV--kF-">
+    </audio>
+    <audio id="cj8"> 
+      <source src="https://drive.google.com/uc?export=download&id=1D4WwUwMVDgrw4yR7y2D7ozlwgdNQ-Do_">
+    </audio>
+    <audio id="cj9"> 
+      <source src="https://drive.google.com/uc?export=download&id=1IqZg1RPI84SivFvi-zZ9ZmHdtbNfCxhP">
+    </audio>
+    <audio id="cj10"> 
+      <source src="https://drive.google.com/uc?export=download&id=1PoOx2OfT9biV2rq_2iEuWPt2vMCkM317">
+    </audio>
+    <audio id="cj11"> 
+      <source src="https://drive.google.com/uc?export=download&id=1wrI1rUZmESDX5l2-xSFUZK5kJnu1dy_9">
+    </audio>
+    <audio id="cj12"> 
+      <source src="https://drive.google.com/uc?export=download&id=1QHu_RwqJ77qaAicCslgE6hanwXIGJd3B">
+    </audio>
+    <audio id="cj13"> 
+      <source src="https://drive.google.com/uc?export=download&id=1LEOCZtQFP0pXDG6HXvpJwRFVc-2epD3h">
+    </audio>
+    <audio id="cj14"> 
+      <source src="https://drive.google.com/uc?export=download&id=1TVEgjOaZoaIY-pDgtOVFVYboJF3Pmpea">
+    </audio>
+    <audio id="cj15"> 
+      <source src="https://drive.google.com/uc?export=download&id=16SvyI0Fvn4fT4VsoUh-euzyZMr1sB4Y3">
+    </audio>
+    <audio id="cj16"> 
+      <source src="https://drive.google.com/uc?export=download&id=10ukVmwgZT65to5OsUY6m2p8ULo4tnXsU">
+    </audio>
+    <audio id="cj17"> 
+      <source src="https://drive.google.com/uc?export=download&id=1J3ibYvIBmvXrAchE7db_lRbBPSzcMDL-">
+    </audio>
 
+    <!-- jeonghu audio -->
+    <audio id="jh1"> 
+      <source src="https://drive.google.com/uc?export=download&id=1PtutAb-85Sf-JZEfV3fafivu72Ku0PZz">
+    </audio>
+    <audio id="jh2"> 
+      <source src="https://drive.google.com/uc?export=download&id=1IOpuWagD_u0P13ScjjDOubOh_qfVbYA2">
+    </audio>
+    <audio id="jh3"> 
+      <source src="https://drive.google.com/uc?export=download&id=1UDY4IbfMXMOK11s6B71qv_cZl9klJMHJ">
+    </audio>
+    <audio id="jh4"> 
+      <source src="https://drive.google.com/uc?export=download&id=1wjM7lL5pYIoRPEehRny3D_QxSU8lvZRS">
+    </audio>
+    <audio id="jh5"> 
+      <source src="https://drive.google.com/uc?export=download&id=1p-GCuJmrTzz4BltDgmApI8KyUXOL4ce8">
+    </audio>
+    <audio id="jh6"> 
+      <source src="https://drive.google.com/uc?export=download&id=1By56WCVGOPjMi-pBoMpjV2VbcHQVl3I0">
+    </audio>
+    <audio id="jh7"> 
+      <source src="https://drive.google.com/uc?export=download&id=1rdv0xsSop5WejH0DLU1vdyYdGnyMPNqS">
+    </audio>
+    <audio id="jh8"> 
+      <source src="https://drive.google.com/uc?export=download&id=1zGNItFZw4ZAyJYji70K4K1rjnx7W2NPs">
+    </audio>
+    <audio id="jh9"> 
+      <source src="https://drive.google.com/uc?export=download&id=1-EbFmQ9aXwXNz4GwD3mCeIlrSBJ7KUi8">
+    </audio>
+    <audio id="jh10"> 
+      <source src="https://drive.google.com/uc?export=download&id=1te6AUT4xB8Ilw5S3mFoBL5kf-w8POHb4">
+    </audio>
+    <audio id="jh11"> 
+      <source src="https://drive.google.com/uc?export=download&id=1AEAnpvWJilN7OL2TZsyFWK_oAQsi56iA">
+    </audio>
+    <audio id="jh12"> 
+      <source src="https://drive.google.com/uc?export=download&id=1xEeVgBvH1tAtu8bGONx3_838fVCD7Lmf">
+    </audio>
+    <audio id="jh13"> 
+      <source src="https://drive.google.com/uc?export=download&id=1wKLZLXbPmLNzpQy-FK--QHvX2bsKj_kU">
+    </audio>
+    <audio id="jh14"> 
+      <source src="https://drive.google.com/uc?export=download&id=17dJXxMAxTmS7onEryG8A5h4g2CMoWvRI">
+    </audio>
+    <audio id="jh15"> 
+      <source src="https://drive.google.com/uc?export=download&id=1TWt2nNzq3LrDOpT-nokA4kKt6XQZPtvb">
+    </audio>
+    <audio id="jh16"> 
+      <source src="https://drive.google.com/uc?export=download&id=1TWt2nNzq3LrDOpT-nokA4kKt6XQZPtvb">
+    </audio>
+    <audio id="jh17"> 
+      <source src="https://drive.google.com/uc?export=download&id=1rn2A5PW2WDTVgJiu69AoyNJrliSR_a4W">
+    </audio>
+
+
+    <div>
+      <img src="https://drive.google.com/uc?export=download&id=13bIOtdx8ghaF-ovmRhtFwFtm6SIk9oiZ" alt="DON'T MISS THE TWIST 3">
+    </div>
 
     <div class="fuck">
       <select id = "chooseVoice">
@@ -50,53 +149,163 @@
         <input type="submit" value="Quit" />
       </form>
     </div>
+
+    <hr>        
+
+    <!-- Page content -->
+    <div class="fuck">
+      <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16"></h3>
+        <div class="w3-row w3-padding-64" id="menu2">
+          <p class="w3-center" style="color:red;"><b>Featured Talents</b></p>
+          <p class="w3-center" style="color:red;">Developers: Lauren Lum and Rujula Kapoor</p>
+          <p class="w3-center" style="color:red;">Voice Actors: uncredited </p>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="nice">
+      <img src="https://drive.google.com/uc?export=download&id=1Yv3XEuwEbUr_pkdPtYmDmKBtHwQ_zlKC" alt="cj sucks" v-on:click="angry">
+    </div>
     
   </div>
 </template>
 
 <script>
+
 function cjtimer() {
-  var ran = Math.floor(Math.random() * 3);
+  var ran = Math.floor(Math.random() * 17);
   var x;
+
+  var voiceOp = document.getElementById("chooseVoice");
+  if(voiceOp.selectedIndex != 0) {
+    return;
+  }
 
   switch(ran) {
     case 0:
-      x = document.getElementById('test1');
-      x.play();
+      x = document.getElementById('cj1');
       break;
     case 1:
-      x = document.getElementById('test2');
-      x.play();
+      x = document.getElementById('cj2');
       break;
     case 2:
-      x = document.getElementById('test3');
-      x.play();
+      x = document.getElementById('cj3');
+      break;
+    case 3:
+      x = document.getElementById('cj4');
+      break;
+    case 4:
+      x = document.getElementById('cj5');
+      break;
+    case 5:
+      x = document.getElementById('cj6');
+      break;
+    case 6:
+      x = document.getElementById('cj7');
+      break;
+    case 7:
+      x = document.getElementById('cj8');
+      break;
+    case 8:
+      x = document.getElementById('cj9');
+      break;
+    case 9:
+      x = document.getElementById('cj10');
+      break;
+    case 10:
+      x = document.getElementById('cj11');
+      break;
+    case 11:
+      x = document.getElementById('cj12');
+      break;
+    case 12:
+      x = document.getElementById('cj13');
+      break;
+    case 13:
+      x = document.getElementById('cj14');
+      break;
+    case 14:
+      x = document.getElementById('cj15');
+      break;
+    case 15:
+      x = document.getElementById('cj16');
+      break;
+    case 16:
+      x = document.getElementById('cj17');
       break;
     default:
       alert('something is big wrong');
   }
+
+  x.play();
 }
 
 function jhtimer() {
-  var ran = Math.floor(Math.random() * 3);
+  var ran = Math.floor(Math.random() * 17);
   var x;
+
+  var voiceOp = document.getElementById("chooseVoice");
+  if(voiceOp.selectedIndex != 1) {
+    return;
+  }
 
   switch(ran) {
     case 0:
       x = document.getElementById('jh1');
-      x.play();
       break;
     case 1:
       x = document.getElementById('jh2');
-      x.play();
       break;
     case 2:
       x = document.getElementById('jh3');
-      x.play();
+      break;
+    case 3:
+      x = document.getElementById('jh4');
+      break;
+    case 4:
+      x = document.getElementById('jh5');
+      break;
+    case 5:
+      x = document.getElementById('jh6');
+      break;
+    case 6:
+      x = document.getElementById('jh7');
+      break;
+    case 7:
+      x = document.getElementById('jh8');
+      break;
+    case 8:
+      x = document.getElementById('jh9');
+      break;
+    case 9:
+      x = document.getElementById('jh10');
+      break;
+    case 10:
+      x = document.getElementById('jh11');
+      break;
+    case 11:
+      x = document.getElementById('jh12');
+      break;
+    case 12:
+      x = document.getElementById('jh13');
+      break;
+    case 13:
+      x = document.getElementById('jh14');
+      break;
+    case 14:
+      x = document.getElementById('jh15');
+      break;
+    case 15:
+      x = document.getElementById('jh16');
+      break;
+    case 16:
+      x = document.getElementById('jh17');
       break;
     default:
       alert('something is big wrong');
   }
+
+  x.play();
 }
 export default {
   name: 'app',
@@ -104,18 +313,16 @@ export default {
   },
   methods: {
     twist() {
-      var timer;
       var voiceOp = document.getElementById("chooseVoice");
 
       if(voiceOp.selectedIndex == 0) {
-        timer = setInterval(cjtimer, 3000);
+        setInterval(cjtimer, 3000);
       }
       else {
         //setInterval(jhtimer, 3000);
-        timer = setInterval(jhtimer, 3000);
+        setInterval(jhtimer, 3000);
       }
 
-      alert(timer);
       
     },
     quit() {
@@ -123,6 +330,10 @@ export default {
       // while(1){
       //     txt = txt += "f";
       // }
+    },
+    angry() {
+       var x = document.getElementById('hoesmad');
+       x.play();
     }
   }
 }
@@ -141,5 +352,22 @@ export default {
 .fuck {
   text-align: center;
   padding: 20px;
+}
+
+.nice {
+  height: 30px;
+  width: 30px;
+}
+
+.nice img {
+  height: 150px;
+  width: 150px;
+  border-radius: 50%;
+  -webkit-transition: -webkit-transform .8s ease-in-out;
+          transition:         transform .8s ease-in-out;
+}
+.nice img:hover {
+  -webkit-transform: rotate(360deg);
+          transform: rotate(360deg);
 }
 </style>
